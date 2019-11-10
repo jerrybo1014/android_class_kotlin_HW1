a990cb76e1438db8b70dc6e675ca4d4fcec79d8c
jerrybo1014 <56484786+jerrybo1014@users.noreply.github.com>
2019-11-10 21:08:59 +0800

    20191110

--- android_class_W1_Q4.kt
+++ android_class_W1_Q4.kt
@@ -1,15 +1,14 @@
-
-
-
 open class Human(var name : String){
-
-   open fun attcak(){
+    open fun attcak(){
         print(name+" use Fist Attack")
     }
-
 }
+/*宣告一個類別Human,變數name,類別內有函數attack()會印出name+" use Fist Attack"*/
+
 
 fun main() {
-    var Human = Human("the name passed in")
+    var Human = Human("name1")
     Human.attcak()
-}
+
+}/*將類別Human存入變數裡，並呼叫函數attack()來列印name+" use Fist Attack"*/
+//name1 use Fist Attack
--- android_class_W1_Q5.kt
+++ android_class_W1_Q5.kt
@@ -1,23 +1,21 @@
-
 open class Human(var name : String) {
 
     open fun attcak() {
         print(name + " use Fist Attack")
     }
 }
+/*宣告一個可被繼承的類別Human,變數name,類別內有函數attack()會印出name+" use Fist Attack"*/
 
 class Mage(name:String) : Human(name){
-
-override fun attcak(){
-    print("$name use Fireball")
+    override fun attcak() {
+        print("$name use Fireball")
+    }
 }
-}
+/*宣告一個類別Mage,變數name,繼承Human,類別內有覆寫函數attack()會印出$name use Fireball*/
 
 
 fun main() {
-    var Mage = Mage("the name passed in")
+    var Mage = Mage("name1")
     Mage.attcak()
-
-
-
-}
+}/*將類別Mage存入變數裡，並呼叫函數attack()來列印$name use Fireball*/
+//name1 use Fireball
